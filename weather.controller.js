699fe3a40ba04efd24d451aac48c84fc09ddbf17
Pamela Zazueta <pamelazazueta@Pamelas-MacBook-Pro.local>
2017-03-20 13:01:24 -0700

    Added search history

--- weather.controller.js
+++ weather.controller.js
@@ -5,18 +5,29 @@
         .module('myApp')
         .controller('weatherController', weatherController);
 
-    weatherController.$inject = [];
+    weatherController.$inject = ['$http'];
 
     /* @ngInject */
-    function weatherController() {
+    function weatherController($http) {
         var vm = this;
         vm.history = [];
-        vm.apiKey =  '519889fb71f27909ea9936375edcae8f'
         vm.search = '';
-      
 
-        function activate() {
+    vm.getInfo = function (search){
+      $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + search + '&units=imperial&appID=519889fb71f27909ea9936375edcae8f')
+      .then(function(response){
+        vm.weather = response.data
 
-        }
+        vm.history.push({
+              name: response.data.name,
+              timestamp: new Date()
+
+        })
+
+      });
+
+    }
+
+
     }
 })();
